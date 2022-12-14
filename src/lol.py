97e0eefd51710782e65ecc992e95a5ad1edcf59f
AndreLeBlanc <leblanc_112@hotmail.com>
2022-12-14 12:58:26 +0100

    bugfix

--- src/lol.py
+++
@@ -1,4 +0,0 @@
-with open("inputs/day3.txt") as file:
-    lines = file.read().splitlines()
-get_priority = lambda x: ord(x)-38 if x.isupper() else ord(x)-96
-print("Part 1\n", len ([get_priority(list(set(line[:int(len(line)/2)]).intersection(set(line[int(len(line)/2):])))[0]) for line in lines]), "\n", "Part 2\n", sum([get_priority(list(set.intersection(*[set(x) for x in lines[n*3:n*3+3]]))[0]) for n in range(int(len(lines)/3))]))
